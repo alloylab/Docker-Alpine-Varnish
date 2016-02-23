78ff5a1572223d06599d28943a738a873f36e13c
Trevor Ferre <trevor@alloylab.com>
2016-02-22 18:50:21 -0600

    Initial Commit

---
+++ conf/start.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+mkdir -p /var/lib/varnish/`hostname` && chown nobody /var/lib/varnish/`hostname`;
+varnishd -s malloc,${VARNISH_MEMORY} -a :80 -b ${VARNISH_BACKEND_ADDRESS}:${VARNISH_BACKEND_PORT};
