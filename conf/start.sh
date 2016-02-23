5c95b12091ea30caf55dab46fe12986e09c8218e
Trevor Ferre <trevor@alloylab.com>
2016-02-22 19:52:40 -0600

    Added VarnishLog to Output
    For logging simplicity

--- conf/start.sh
+++ conf/start.sh
@@ -2,3 +2,5 @@
 
 mkdir -p /var/lib/varnish/`hostname` && chown nobody /var/lib/varnish/`hostname`;
 varnishd -s malloc,${VARNISH_MEMORY} -a :80 -b ${VARNISH_BACKEND_ADDRESS}:${VARNISH_BACKEND_PORT};
+sleep 1;
+varnishlog;
